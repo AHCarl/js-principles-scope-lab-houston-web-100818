496dff5adba246f0312d5ffffec9cf073f99d118
Alexander Carlin <ahcarlin@gmail.com>
2018-11-16 15:23:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 var customerName = 'bob';
 
 function upperCaseCustomerName(){
- return customerName.toUpperCase();
+ let kong = customerName.toUpperCase();
+ return kong;
 }
 
 function setBestCustomer() {
