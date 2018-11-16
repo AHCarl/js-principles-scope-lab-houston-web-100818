9959041aeb46787695e23b205456fb0dafbdf526
Alexander Carlin <ahcarlin@gmail.com>
2018-11-16 15:17:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+var customerName = "Bob Saget";
+
+function upperCaseCustomerName{
+  return customerName.toUpperCase()
+}
+
+function setBestCustomer {
+  bestCustomer = 'not bob'
+}
+
